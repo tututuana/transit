877f5fcd58c0b4b8f3f093e4edc4f4cac12fc00e
Beta Pictoris <git@mrhallway.me>
2021-08-05 17:46:10 -0700

    📝 Code comments

--- src/ftp.py
+++ src/ftp.py
@@ -1,6 +1,6 @@
 from ftplib import FTP
 
-
+# Connect to a remote FTP server
 def connect(server, username='anonymous', password=''):
     global ftp
     ftp = FTP(server)
@@ -11,23 +11,28 @@ def connect(server, username='anonymous', password=''):
 
     return(files)
 
+# Get welcome message from FTP server
 def welcome():
     welcome = ftp.getwelcome()
     return(welcome)
 
+# Get file listing from FTP server
 def ls():
     files = ftp.nlst()
 
     return(files)
 
+# Change remote dir 
 def cd(dir):
     ftp.cwd(dir)
     
     return(ls())
 
+# Quit connection
 def bye():
     ftp.quit()
 
+# Download file from remote FTP server.
 def download(filename):
     with open(str(filename), 'wb') as fp:
         ftp.retrbinary('RETR ' + str(filename), fp.write)
